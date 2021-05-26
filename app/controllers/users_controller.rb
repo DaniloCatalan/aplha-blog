f49b038c57cbebdaad3bab0afd0f2cbb8671c922
Danilo P Catalan <danilo.e.catalan@gmail.com>
2021-05-26 15:10:04 -0400

    Add delete user and dependent articles destroy functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
     @pagy, @articles = pagy(@user.articles.all, items: 5)
@@ -22,7 +22,6 @@ class UsersController < ApplicationController
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to @user
-      
     else
       render 'edit'
     end
@@ -37,6 +36,13 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
   end
 
   private
